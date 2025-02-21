9452fd8274cd20c93a207e38262447aa1cc40520
ickshonpe <david.curthoys@googlemail.com>
2025-02-21 18:12:50 +0000

    Removed unused imports

--- crates/bevy_ui/src/render/ui_texture_slice_pipeline.rs
+++ crates/bevy_ui/src/render/ui_texture_slice_pipeline.rs
@@ -13,13 +13,11 @@ use bevy_ecs::{
 use bevy_image::prelude::*;
 use bevy_math::{FloatOrd, Mat4, Rect, Vec2, Vec4Swizzles};
 use bevy_platform_support::collections::HashMap;
-use bevy_render::sync_world::MainEntity;
 use bevy_render::{
     render_asset::RenderAssets,
     render_phase::*,
     render_resource::{binding_types::uniform_buffer, *},
     renderer::{RenderDevice, RenderQueue},
-    sync_world::TemporaryRenderEntity,
     texture::{GpuImage, TRANSPARENT_IMAGE_HANDLE},
     view::*,
     Extract, ExtractSchedule, Render, RenderSet,
