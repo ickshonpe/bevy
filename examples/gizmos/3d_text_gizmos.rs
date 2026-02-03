1f671b1241b61d6a8ee5317e5fbb78541764fb2c
ickshonpe <david.curthoys@googlemail.com>
2026-02-03 20:13:18 +0000

    renamed text_gizmos example to 2d_text_gizmos
    Added new example 3d_text_gizmos

---
+++ examples/gizmos/3d_text_gizmos.rs
@@ -0,0 +1,46 @@
+//! Basic example demonstrating 3d text gizmos
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .add_systems(Update, hello_world)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+    ));
+}
+
+fn hello_world(mut text_gizmos: Gizmos, time: Res<Time>) {
+    let t = 0.2 * time.elapsed_secs();
+
+    text_gizmos.text(
+        Isometry3d::new(Vec3::new(0.0, 1.5, 0.0), Quat::from_rotation_y(-t)),
+        "Hello",
+        1.,
+        Vec2::ZERO,
+        Color::WHITE,
+    );
+
+    text_gizmos.text(
+        Isometry3d::new(Vec3::new(0.0, 0.0, 0.0), Quat::from_rotation_y(-t - 0.25)),
+        "Text",
+        1.,
+        Vec2::ZERO,
+        Color::WHITE,
+    );
+
+    text_gizmos.text(
+        Isometry3d::new(Vec3::new(0.0, -1.5, 0.0), Quat::from_rotation_y(-t - 0.5)),
+        "Gizmos",
+        1.,
+        Vec2::ZERO,
+        Color::WHITE,
+    );
+}
