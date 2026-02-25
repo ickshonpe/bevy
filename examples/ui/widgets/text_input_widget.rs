1849500d966efef14132e23ddf9a599441098fdb
ickshonpe <david.curthoys@googlemail.com>
2026-02-25 11:01:02 +0000

    Added skeleton text_input_widget example and cargo entry

---
+++ examples/ui/widgets/text_input_widget.rs
@@ -0,0 +1,14 @@
+//! Text input example
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
