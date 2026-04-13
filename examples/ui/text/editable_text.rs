5160710cc1550a527971c881b4aa169c97cda446
ickshonpe <david.curthoys@googlemail.com>
2026-04-13 12:25:01 +0100

    Removed more unused imports

--- examples/ui/text/editable_text.rs
+++ examples/ui/text/editable_text.rs
@@ -11,7 +11,7 @@ use bevy::input_focus::{
     InputFocus,
 };
 use bevy::prelude::*;
-use bevy::text::{EditableText, FontCx, LayoutCx, TextCursorStyle};
+use bevy::text::{EditableText, TextCursorStyle};
 
 fn main() {
     App::new()
