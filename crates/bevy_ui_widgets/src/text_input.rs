8b2c998a2ade2cf99babbbeeafc53e03f646766a
ickshonpe <david.curthoys@googlemail.com>
2026-04-28 23:26:27 +0100

    Added click count tracking to Click events. Added SelectWordAtPoint TextEdit. Added on_pointer_click observer to text_input. On double and triple click events queue appropriate TextEdits

--- crates/bevy_ui_widgets/src/text_input.rs
+++ crates/bevy_ui_widgets/src/text_input.rs
@@ -15,7 +15,7 @@ use bevy_input_focus::{
     FocusCause, FocusGained, FocusLost, FocusedInput, InputFocus, InputFocusSystems,
 };
 use bevy_math::Vec2;
-use bevy_picking::events::{Drag, Pointer, Press, Release};
+use bevy_picking::events::{Click, Drag, Pointer, Press, Release};
 use bevy_picking::pointer::PointerButton;
 use bevy_text::{EditableText, PreeditCursor, TextEdit};
 use bevy_ui::widget::{scroll_editable_text, update_editable_text_layout, TextScroll};
@@ -201,6 +201,56 @@ fn on_pointer_press(
     input_focus.set(press.entity, FocusCause::Pressed);
 
     press.propagate(false);
+}
+
+/// System that processes pointer click events into text edit actions for [`EditableText`] widgets.
+///
+/// Note that this does not immediately apply the edits; they are queued up in [`EditableText::pending_edits`],
+/// and then applied later by the [`apply_text_edits`](`bevy_text::apply_text_edits`) system.
+fn on_pointer_click(
+    mut click: On<Pointer<Click>>,
+    mut text_input_query: Query<(
+        &mut EditableText,
+        &ComputedNode,
+        &ComputedUiRenderTargetInfo,
+        &UiGlobalTransform,
+        &TextScroll,
+    )>,
+    ui_scale: Res<UiScale>,
+) {
+    if click.button != PointerButton::Primary {
+        return;
+    }
+
+    let Ok((mut editable_text, node, target, transform, text_scroll)) =
+        text_input_query.get_mut(click.entity)
+    else {
+        return;
+    };
+
+    if editable_text.is_composing() {
+        // The IME is active; all input needs to be routed there, including pointer multi-clicks.
+        return;
+    }
+
+    let Some(local_pos) = transform.try_inverse().map(|inverse| {
+        inverse
+            .transform_point2(click.pointer_location.position * target.scale_factor() / ui_scale.0)
+            - node.content_box().min
+            + text_scroll.0
+    }) else {
+        return;
+    };
+
+    println!("click count: {}", click.count);
+
+    match click.count {
+        2 => editable_text.queue_edit(TextEdit::SelectWordAtPoint(local_pos)),
+        3 => editable_text.queue_edit(TextEdit::SelectAll),
+        _ => {}
+    }
+
+    click.propagate(false);
 }
 
 /// System that processes pointer drag events into text edit actions for [`EditableText`] widgets.
@@ -488,6 +538,7 @@ impl Plugin for EditableTextInputPlugin {
             .add_observer(on_pointer_press)
             .add_observer(on_focus_lost_clear_ime)
             .add_observer(on_focus_select_all)
+            .add_observer(on_pointer_click)
             .add_systems(
                 PreUpdate,
                 (
